bdc76f99f6b0131b6ddf876f0aa2fdb1bd802ce1
real-chiu <real.chiu@oocl.com>
2020-04-15 17:11:54 +0800

    PostMapping to add employee; Employee controller data initialization;

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -11,13 +11,23 @@ import java.util.List;
 public class EmployeeController {
     List<Employee> employees = new ArrayList<>();
 
-    @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<Employee> getAllEmployees() {
+    public EmployeeController() {
         employees.add(new Employee(0, "Xiaoming", 20, "Male"));
         employees.add(new Employee(1, "Xiaohong", 19, "Male"));
         employees.add(new Employee(2, "Xiaozhi", 15, "Male"));
         employees.add(new Employee(3, "Xiaoxia", 16, "Female"));
+    }
+    
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Employee> getAllEmployees() {
         return employees;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createNewEmployee(@RequestBody Employee employee) {
+        employees.add(employee);
+        return employee;
     }
 }
