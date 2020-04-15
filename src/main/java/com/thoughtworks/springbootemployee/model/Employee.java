8e298126ae0e3de6ea85f4b210647791d78f4512
real-chiu <real.chiu@oocl.com>
2020-04-15 20:00:10 +0800

    add getter setter for salary

--- src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
@@ -14,6 +14,14 @@ public class Employee {
         this.gender = gender;
         this.salary = salary;
     }
+    
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 
     public void setId(int id) {
         this.id = id;
