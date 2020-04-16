c2e27da55318d774f0e8fb9c5ac24cb8dbafef4c
real-chiu <real.chiu@oocl.com>
2020-04-16 15:01:45 +0800

    passed test case shouldAbleToFindAllCompany

--- src/test/java/com/thoughtworks/springbootemployee/CompanyControllerTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyControllerTest.java
@@ -5,6 +5,7 @@ import com.thoughtworks.springbootemployee.controller.EmployeeController;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import io.restassured.http.ContentType;
+import io.restassured.mapper.TypeRef;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import io.restassured.module.mockmvc.response.MockMvcResponse;
 import org.junit.Assert;
@@ -12,6 +13,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 
@@ -30,5 +34,24 @@ public class CompanyControllerTest {
         Company company = mockResponse.getBody().as(Company.class);
         Assert.assertEquals(1, company.getId());
         Assert.assertEquals("CargoSmart", company.getCompanyName());
+    }
+
+    @Test
+    public void shouldAbleToFindAllCompany() {
+        RestAssuredMockMvc.standaloneSetup(new CompanyController());
+        MockMvcResponse mockResponse = given().contentType(ContentType.JSON)
+                .when()
+                .get("/companies");
+        Assert.assertEquals(200, mockResponse.getStatusCode());
+
+        List<Company> companies = mockResponse.getBody().as(new TypeRef<List<Company>>() {
+            @Override
+            public Type getType() {
+                return super.getType();
+            }
+        });
+        Assert.assertEquals(2, companies.size());
+        Assert.assertEquals("OOCL", companies.get(0).getCompanyName());
+        Assert.assertEquals("CargoSmart", companies.get(1).getCompanyName());
     }
 }
