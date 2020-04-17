3a567fa4b7cffec3001070a285e502fe8acc604f
real-chiu <real.chiu@oocl.com>
2020-04-17 15:29:47 +0800

    parkingboy oneToOne mapping to employee

---
+++ src/main/java/com/thoughtworks/springbootemployee/model/ParkingBoy.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.springbootemployee.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "parking_boy")
+public class ParkingBoy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nickname;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    private Employee employee;
+
+}
