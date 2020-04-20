4f5d8d4a80d1857789c0af02ab4cc7a24de578c9
real-chiu <real.chiu@oocl.com>
2020-04-20 17:21:27 +0800

    Company Employee one-to-many; Employee Parking_boy one to one;

--- src/main/resources/db/migration/V202004201623__Add_Employee_Company_and_ParkingBoy_table.sql
+++ src/main/resources/db/migration/V202004201623__Add_Employee_Company_and_ParkingBoy_table.sql
@@ -2,13 +2,20 @@ CREATE TABLE IF NOT EXISTS Parking_boy(
     id int AUTO_INCREMENT PRIMARY KEY,
     nickname varchar(30)
 );
+CREATE TABLE IF NOT EXISTS Company(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    company_name varchar(30),
+    employees_number int
+);
 CREATE TABLE IF NOT EXISTS Employee(
     id int AUTO_INCREMENT PRIMARY KEY,
-    employee_name varchar(30),
+    name varchar(30),
     age int,
     gender varchar(30),
     salary int,
     company_id int,
     parking_boy_id int,
+    FOREIGN KEY (company_id) REFERENCES Company(id),
     FOREIGN KEY (parking_boy_id) REFERENCES Parking_boy(id)
+
 );
